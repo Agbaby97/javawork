633f61ba06ac7156df78bd0363552bda4ac905b5
starlkhorm <degeneralola@gmail.com>
2025-05-19 16:32:27 +0100

    commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// console.log(200)
+
+//write arrow function that devide two Number
+// let division = () => {
+//     let num1 = prompt("enter any number");
+//     let num2 = prompt ("enter number");
+// console.log(num1 / num2);
+// }
+// //function
+// const checkEvenOrOdd = function (a){
+//     if (a % 2 === 0) {
+//         alert(`${a}is an even number`);
+//     }else if ( a % 2 !== ){
+//         alert(`${a}is an odd number`);
+//     }else{
+//         alert("sorry there is an incorrect input somewhere");
+//     }
+// };
+// a = parseInt(prompt("enter your numbrer"));
+// checkEvenOrOdd(a);
+
+
+// //write a function that returns the largest of three number
+// function number () {
+
+// let num1 = prompt("enter  first number")
+// let num2 = prompt("enter second number")
+// let num3 =prompt("enter third number")
+// console.log(Math.max(num1, num2, num3));
+// }
+
+// number()
+
+// const checkEven
+
+// function reverse (){
+//     let result = prompt("enter a number")
+//     console.log(back.split('').reverse('').join(''));
+// }
+// reverse()
+
